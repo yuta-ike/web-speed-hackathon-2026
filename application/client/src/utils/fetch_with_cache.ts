bedc5fa284dd970d8d4e08d2363ead1589e22d39
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2026-03-20 17:48:42 +0900

    - negaposiにクライアントキャッシュを実装

--- application/client/src/utils/fetch_with_cache.ts
+++ application/client/src/utils/fetch_with_cache.ts
@@ -1,10 +1,13 @@
 import { fetchJSON } from "./fetchers";
 
-const cacheMap = new Map<string, { suggestions: string[]; tokens: string[] }>();
+const suggestionsCacheMap = new Map<
+  string,
+  { suggestions: string[]; tokens: string[] }
+>();
 
 export const fetchSuggestions = async (query: string) => {
-  if (cacheMap.has(query)) {
-    return cacheMap.get(query)!;
+  if (suggestionsCacheMap.has(query)) {
+    return suggestionsCacheMap.get(query)!;
   }
 
   const result = await fetchJSON<{
@@ -12,23 +15,39 @@ export const fetchSuggestions = async (query: string) => {
     tokens: string[];
   }>(`/api/v1/crok/suggestions?q=${encodeURIComponent(query)}`);
 
-  cacheMap.set(query, result);
-  if (cacheMap.size > 100) {
+  suggestionsCacheMap.set(query, result);
+  if (suggestionsCacheMap.size > 100) {
     // キャッシュが100件を超えたら古いものから削除
-    const firstKey = cacheMap.keys().next().value;
+    const firstKey = suggestionsCacheMap.keys().next().value;
     if (firstKey) {
-      cacheMap.delete(firstKey);
+      suggestionsCacheMap.delete(firstKey);
     }
   }
 
   return result;
 };
 
+const negaposiCacheMap = new Map<
+  string,
+  { label: "positive" | "negative" | "neutral" }
+>();
 export const fetchNegaposi = async (query: string) => {
+  if (negaposiCacheMap.has(query)) {
+    return negaposiCacheMap.get(query)!;
+  }
+
   const result = await fetchJSON<{
-    score: number;
     label: "positive" | "negative" | "neutral";
   }>(`/api/v1/negaposi?q=${encodeURIComponent(query)}`);
+
+  negaposiCacheMap.set(query, result);
+  if (negaposiCacheMap.size > 100) {
+    // キャッシュが100件を超えたら古いものから削除
+    const firstKey = negaposiCacheMap.keys().next().value;
+    if (firstKey) {
+      negaposiCacheMap.delete(firstKey);
+    }
+  }
 
   return result;
 };
