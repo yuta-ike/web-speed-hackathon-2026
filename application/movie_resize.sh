dbf394ea2b63eebbea2e98255669ddf47da03846
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2026-03-20 22:37:27 +0900

    - 動画のサイズを圧縮

--- application/movie_resize.sh
+++ application/movie_resize.sh
@@ -6,7 +6,7 @@ for f in "$TARGET_DIR"/*.gif; do
     thumb_out="$TARGET_DIR/${basename}_thumb.webp"
 
     # 1. メイン：WebM動画 (横640px, VP9, 音声なし)
-    ffmpeg -y -i "$f" -c:v libvpx-vp9 -b:v 0 -crf 30 -vf "scale=640:-1" -an "$webm_out"
+    ffmpeg -y -i "$f" -c:v libvpx-vp9 -b:v 0 -crf 30 -vf "scale=480:-1" -an "$webm_out"
 
 
     echo "$f の変換が完了しました。"
