a1ec57931c517e2dd04f3e5db3a8563d409bbfe5
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2026-03-21 18:30:29 +0900

    fix

--- application/client/src/components/new_post_modal/NewPostModalPage.tsx
+++ application/client/src/components/new_post_modal/NewPostModalPage.tsx
@@ -157,21 +157,51 @@ export const NewPostModalPage = ({
         <AttachFileInputButton
           accept="image/*"
           active={params.images.length !== 0}
-          icon={<FontAwesomeIcon iconType="images" styleType="solid" />}
+          icon={
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 576 512"
+              fill="currentColor"
+              className="inline"
+            >
+              <path d="M480 416v16c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V176c0-26.51 21.49-48 48-48h16v208c0 44.112 35.888 80 80 80h336zm96-80V80c0-26.51-21.49-48-48-48H144c-26.51 0-48 21.49-48 48v256c0 26.51 21.49 48 48 48h384c26.51 0 48-21.49 48-48zM256 128c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-96 144l55.515-55.515c4.686-4.686 12.284-4.686 16.971 0L272 256l135.515-135.515c4.686-4.686 12.284-4.686 16.971 0L512 208v112H160v-48z"></path>
+            </svg>
+          }
           label="画像を添付"
           onChange={handleChangeImages}
         />
         <AttachFileInputButton
           accept="audio/*"
           active={params.sound !== undefined}
-          icon={<FontAwesomeIcon iconType="music" styleType="solid" />}
+          icon={
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+              className="inline"
+            >
+              <path d="M470.38 1.51L150.41 96A32 32 0 0 0 128 126.51v261.41A139 139 0 0 0 96 384c-53 0-96 28.66-96 64s43 64 96 64 96-28.66 96-64V214.32l256-75v184.61a138.4 138.4 0 0 0-32-3.93c-53 0-96 28.66-96 64s43 64 96 64 96-28.65 96-64V32a32 32 0 0 0-41.62-30.49z"></path>
+            </svg>
+          }
           label="音声を添付"
           onChange={handleChangeSound}
         />
         <AttachFileInputButton
           accept="video/*"
           active={params.movie !== undefined}
-          icon={<FontAwesomeIcon iconType="video" styleType="solid" />}
+          icon={
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 576 512"
+              fill="currentColor"
+              className="inline"
+            >
+              <path d="M336.2 64H47.8C21.4 64 0 85.4 0 111.8v288.4C0 426.6 21.4 448 47.8 448h288.4c26.4 0 47.8-21.4 47.8-47.8V111.8c0-26.4-21.4-47.8-47.8-47.8zm189.4 37.7L416 177.3v157.4l109.6 75.5c21.2 14.6 50.4-.3 50.4-25.8V127.5c0-25.4-29.1-40.4-50.4-25.8z"></path>
+            </svg>
+          }
           label="動画を添付"
           onChange={handleChangeMovie}
         />
